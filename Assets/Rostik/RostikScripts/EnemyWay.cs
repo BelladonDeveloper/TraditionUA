6a89535513cecfd3191a3450b55b83cf81cea012
KWAZARD <rostik071128@gmail.com>
2024-03-03 08:37:41 +0200

    EnemyWay

---
+++ Assets/Rostik/RostikScripts/EnemyWay.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EnemyWay : MonoBehaviour
+{
+
+    public Transform[] patrolPoints;
+    public int targetPoint;
+    public float speed;
+
+    void Start()
+    {
+        targetPoint = 0;
+    }
+
+    void Update()
+    {
+        if (transform.position == patrolPoints[targetPoint].position)
+        {
+            increaseTargetInt();
+        }
+        transform.position = Vector3.MoveTowards(transform.position, patrolPoints[targetPoint].position, speed * Time.deltaTime);
+
+    }
+    void increaseTargetInt()
+    {
+        targetPoint++;
+        if (targetPoint >= patrolPoints.Length)
+        {
+            targetPoint = 0;
+        }
+    }
+}
+
