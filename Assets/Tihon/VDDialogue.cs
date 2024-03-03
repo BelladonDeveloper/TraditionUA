1482e98da413e8f73f636f55a499784d4ff2e886
Neonman13 <110163483+Neonman13@users.noreply.github.com>
2024-03-03 10:23:45 +0100

    ....

--- Assets/Tihon/VDDialogue.cs
+++ Assets/Tihon/VDDialogue.cs
@@ -23,6 +23,15 @@ public class VDDialogue : MonoBehaviour
     public string phrase7;
     public string phrase8;
     public int phrasesCount = 0;
+    public string phrase11;
+    public string phrase12;
+    public string phrase13;
+    public string phrase14;
+    public string phrase15;
+    public string phrase16;
+    public string phrase17;
+    public string phrase18;
+    public int meeting = 1;
     public string lvl;
 
     public void Start()
@@ -57,19 +66,35 @@ public class VDDialogue : MonoBehaviour
         Debug.Log(1);
         if(phrasesCount > 0)
         {
-            if(phrasesCount== 1) { script = phrase1; }
-            if(phrasesCount== 2) { script = phrase2; }
-            if(phrasesCount== 3) { script = phrase3; }
-            if(phrasesCount== 4) { script = phrase4; }
-            if(phrasesCount== 5) { script = phrase5; }
-            if(phrasesCount== 6) { script = phrase6; }
-            if(phrasesCount== 7) { script = phrase7; }
-            if(phrasesCount== 8) { script = phrase8; }
-            phrasesCount--;
+            if(meeting == 1)
+            {
+                if (phrasesCount == 1) { script = phrase1; }
+                if (phrasesCount == 2) { script = phrase2; }
+                if (phrasesCount == 3) { script = phrase3; }
+                if (phrasesCount == 4) { script = phrase4; }
+                if (phrasesCount == 5) { script = phrase5; }
+                if (phrasesCount == 6) { script = phrase6; }
+                if (phrasesCount == 7) { script = phrase7; }
+                if (phrasesCount == 8) { script = phrase8; }
+                phrasesCount--;
+            }
+            else
+            {
+                if (phrasesCount == 1) { script = phrase11; }
+                if (phrasesCount == 2) { script = phrase12; }
+                if (phrasesCount == 3) { script = phrase13; }
+                if (phrasesCount == 4) { script = phrase14; }
+                if (phrasesCount == 5) { script = phrase15; }
+                if (phrasesCount == 6) { script = phrase16; }
+                if (phrasesCount == 7) { script = phrase17; }
+                if (phrasesCount == 8) { script = phrase18; }
+                phrasesCount--;
+            }
         }
         else
         {
             SceneManager.LoadScene(lvl);
+            meeting++;
             Time.timeScale = 1;
             bubble.GetComponent<CanvasGroup>().alpha = 0;
             joystick.GetComponent<CanvasGroup>().alpha = 1;
