029b512de617031a816aadfc16c77733f6bb3ec6
Mega_prase_z_Česka <animecibupeli@gmail.com>
2024-03-08 02:39:28 +0200

    Some Level stuff

--- Assets/Scenes/ukrainian independence day Stuff/Garbage for prototype/Scripts/CarTimer.cs
+++ Assets/Scenes/ukrainian independence day Stuff/Garbage for prototype/Scripts/CarTimer.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class CarTimer : MonoBehaviour
 {
+    [SerializeField] private CameraFollow _camFollow;
+
+    [SerializeField] private Transform destination;
+    [SerializeField] private GameObject Player;
     [SerializeField] private GameObject _car;
     [SerializeField] private GameObject _woll;
 
@@ -12,6 +16,7 @@ public class CarTimer : MonoBehaviour
 
     public float countdownTime; 
     private bool timerActive = false;
+    private bool isFirstTime = false;
 
     private void Update()
     {
@@ -27,7 +32,20 @@ public class CarTimer : MonoBehaviour
         }
     }
 
-    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (isFirstTime == false)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                _camFollow.Fade();
+                Player = collision.gameObject;
+                StartCoroutine(WaitForFade());
+            }
+        }
+    }
+
+
     private void TimerEnded()
     {
         transform.DOMoveZ(targetPositionZ, 4).SetEase(Ease.Linear).OnComplete(HideWoll);
@@ -42,5 +60,17 @@ public class CarTimer : MonoBehaviour
     public void StartTimer()
     {
         timerActive = true; 
+    }
+
+    IEnumerator WaitForFade()
+    {
+        yield return new WaitForSeconds(2f);
+        _car.transform.position = destination.position;
+        _car.transform.rotation = destination.rotation;
+        Player.transform.position = new Vector3(destination.position.x, destination.position.y, destination.position.z + 4);
+        _camFollow.Light();
+        isFirstTime = true;
+        yield return new WaitForSeconds(0.5f);
+        transform.DOMoveX(-70, 2).SetEase(Ease.Linear);
     }
 }
